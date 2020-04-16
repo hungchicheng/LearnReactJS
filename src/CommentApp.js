65aa669a071c3ba1777a6a37b8f0ece594da6911
tommy.cheng <tommy.cheng@skylinetw.com>
2020-04-16 15:14:23 +0800

    add pratice 2

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CommentInput from "./CommentInput";
 import CommentList from "./CommentList";
+import PropTypes from "prop-types";
 
 class CommentApp extends Component {
     constructor() {
@@ -10,18 +11,47 @@ class CommentApp extends Component {
         };
     }
 
+    componentWillMount() {
+        this._loadComments();
+    }
+
+    _loadComments() {
+        let comments = localStorage.getItem("comments");
+        if (comments) {
+            comments = JSON.parse(comments);
+            this.setState({ comments });
+        }
+    }
+
+    _saveComments(comments) {
+        localStorage.setItem("comments", JSON.stringify(comments));
+    }
+
     handleSubmitComment(comment) {
-        this.state.comments.push(comment)
-        this.setState({
-          comments: this.state.comments
-        })
+        if (!comment) return;
+        if (!comment.username) return alert("请输入用户名");
+        if (!comment.content) return alert("请输入评论内容");
+        const comments = this.state.comments;
+        comments.push(comment);
+        this.setState({ comments });
+        this._saveComments(comments);
+    }
+
+    handleDeleteComment(index) {
+        const comments = this.state.comments;
+        comments.splice(index, 1);
+        this.setState({ comments });
+        this._saveComments(comments);
     }
 
     render() {
         return (
             <div className="wrapper">
                 <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-                <CommentList comments={this.state.comments}/>
+                <CommentList
+                    comments={this.state.comments}
+                    onDeleteComment={this.handleDeleteComment.bind(this)}
+                />
             </div>
         );
     }
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
 
 class CommentInput extends Component {
+    static propTypes = {
+        onSubmit: PropTypes.func,
+    };
+
     constructor() {
         super();
         this.state = {
             username: "",
             content: "",
         };
+    }
+
+    componentDidMount() {
+        this.textarea.focus();
+    }
+
+    _loadUsername() {
+        const username = localStorage.getItem("username");
+        if (username) {
+            this.setState({ username });
+        }
+    }
+
+    _saveUsername(username) {
+        localStorage.setItem("username", username);
+    }
+
+    handleUsernameBlur(event) {
+        this._saveUsername(event.target.value);
     }
 
     handleUsernameChange(event) {
@@ -23,10 +47,13 @@ class CommentInput extends Component {
 
     handleSubmit(event) {
         if (this.props.onSubmit) {
-            const { username, content } = this.state;
-            this.props.onSubmit({ username, content });
-        }
-        this.setState({ content: "" });
+            this.props.onSubmit({
+              username: this.state.username,
+              content: this.state.content,
+              createdTime: +new Date()
+            })
+          }
+          this.setState({ content: '' })
     }
 
     render() {
@@ -37,6 +64,7 @@ class CommentInput extends Component {
                     <div className="comment-field-input">
                         <input
                             value={this.state.username}
+                            onBlur={this.handleUsernameBlur.bind(this)}
                             onChange={this.handleUsernameChange.bind(this)}
                         />
                     </div>
@@ -45,6 +73,7 @@ class CommentInput extends Component {
                     <span className="comment-field-name">评论内容：</span>
                     <div className="comment-field-input">
                         <textarea
+                            ref={(textarea) => (this.textarea = textarea)}
                             value={this.state.content}
                             onChange={this.handleContentChange.bind(this)}
                         />
